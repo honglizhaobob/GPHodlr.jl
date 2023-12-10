5b9a5a26351f26c003d2530353cf5e3b81e6d47d
Documenter.jl <documenter@juliadocs.github.io>
2023-12-10 02:15:55 +0000

    build based on 6a5fe19

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GPHodlr","category":"page"},{"location":"#GPHodlr","page":"Home","title":"GPHodlr","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GPHodlr.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GPHodlr]","category":"page"}]
+}
